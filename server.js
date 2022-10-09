463ae04bf57d42d2fca16a4a99060a5501e3f7bc
Caitlin O'Reilly <coreilly29@gmail.com>
2022-10-08 22:07:40 -0400

    finished adding CRUD routes

--- server.js
+++ server.js
@@ -4,6 +4,9 @@ const mysql = require("mysql2");
 // import express
 const express = require("express");
 
+// import module for inputCheck
+const inputCheck = require("./utils/inputCheck");
+
 // add PORT designation and initialze the app
 const PORT = process.env.PORT || 3001;
 const app = express();
@@ -71,12 +74,12 @@ app.get("/api/candidate/:id", (req, res) => {
 
 // Delete a candidate
 // use the HTTP request method delete()
-// endpoint here also includes a route parameter to uniquely identify the candidate to remove 
-// using prepared SQL statement w/ a placeholder 
-// assign req.params.id to params like in the last route 
+// endpoint here also includes a route parameter to uniquely identify the candidate to remove
+// using prepared SQL statement w/ a placeholder
+// assign req.params.id to params like in the last route
 // json object route response will be the message 'deleted' with the changes property set to result.affectedRows - verifies if any rows were changed
-// what is user tries to delete candidate that does not exist? --> else if statement 
-// if no affectedRows as a result of the delete query, that means no candidate by that id 
+// what is user tries to delete candidate that does not exist? --> else if statement
+// if no affectedRows as a result of the delete query, that means no candidate by that id
 // therefore return an approp message to the client "candidate not found"
 app.delete("/api/candidate/:id", (req, res) => {
   const sql = `DELETE FROM candidates WHERE id = ?`;
@@ -100,16 +103,41 @@ app.delete("/api/candidate/:id", (req, res) => {
 });
 
 // Create a candidate
-// const sql = `INSERT INTO candidates (id, first_name, last_name, industry_connected)
-//               VALUES (?,?,?,?)`;
-// const params = [1, "Ronald", "Firbank", 1];
+// http request method post() to insert a candidate into the candidates table - use endpoint /api/candidates
+// in callback function, use object req.body to populate the candidate's data (using object destructuring to pull body property out of the request object)
+// assign errors to receive the return from the inputCheck function
+// validate user data before changes inserted into the DB
+// if inputCheck() function returns error, an err message is returned to client as a 400 status code to prompt for diff user request w/ json obj that contains the reasons for the error
+// import module at top of file
+app.post("/api/candidate", ({ body }, res) => {
+  const errors = inputCheck(
+    body,
+    "first_name",
+    "last_name",
+    "industry_connected"
+  );
+  if (errors) {
+    res.status(400).json({ error: errors });
+    return;
+  }
+  // add the database call inside the POST routine below if (errors) block
+  // MySQL will autogenerate id, params assignment has 3 elements in its array that contains the user data collected in req.body
+  //use query() method to execute prepared sql statement - we send the response using res.json() method w/ a success message & user data that was used to create the new data entry
+  const sql = `INSERT INTO candidates (first_name, last_name, industry_connected)
+    VALUES (?,?,?)`;
+  const params = [body.first_name, body.last_name, body.industry_connected];
 
-// db.query(sql, params, (err, result) => {
-//   if (err) {
-//     console.log(err);
-//   }
-//   console.log(result);
-// });
+  db.query(sql, params, (err, result) => {
+    if (err) {
+      res.status(400).json({ error: err.message });
+      return;
+    }
+    res.json({
+      message: "success",
+      data: body,
+    });
+  });
+});
 
 // add a route to handle user requests that are not supported by the app
 // default response for any other request (Not Found)
