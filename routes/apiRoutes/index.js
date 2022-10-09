9a3149ad76d33ee07a03c7e6da38a0122933099f
Caitlin O'Reilly <coreilly29@gmail.com>
2022-10-09 18:33:17 -0400

    working on creating API route to add new votes

--- routes/apiRoutes/index.js
+++ routes/apiRoutes/index.js
@@ -4,5 +4,7 @@ const router = express.Router();
 router.use(require("./candidateRoutes"));
 router.use(require("./partyRoutes"));
 router.use(require("./voterRoutes"));
+// tell router object to use the votes route alongside the others
+router.use(require("./voteRoutes"));
 
 module.exports = router;
